16f22db745e310767c9aec0ac8a52b7c949a94e3
Mauro Pinto <Mauro_Pinto@iscte.pt>
2017-10-05 23:34:02 +0100

    initial commit

---
+++ esi/src/main/java/esi/App.java
@@ -0,0 +1,11 @@
+package esi;
+
+public class App {
+	public String sample() {
+		return "Sample";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new App().sample());
+	}
+}
---
+++ esi/src/test/java/esi/AppTest.java
@@ -0,0 +1,14 @@
+package esi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals("Sample", new App().sample());
+	}
+
+}
