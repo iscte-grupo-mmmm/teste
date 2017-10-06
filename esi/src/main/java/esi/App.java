0309d7f9d863d366db417dedc140931829c3d784
Michael Carvalho <mrmco@iscte-iul.pt>
2017-10-06 12:55:34 +0100

    Added new Features

--- esi/src/main/java/esi/App.java
+++ esi/src/main/java/esi/App.java
@@ -4,8 +4,18 @@ public class App {
 	public String sample() {
 		return "Sample 2";
 	}
+	
+	public String sampleMike() {
+		return "Mike Sample";
+	}
+	
+	public String sampleMiguel() {
+		return "OutSystems dude - Is that a game?";
+	}
 
 	public static void main(String[] args) {
 		System.out.println(new App().sample());
+		System.out.println(new App().sampleMike());
+		System.out.println(new App().sampleMiguel());
 	}
 }
--- esi/src/test/java/esi/AppTest.java
+++ esi/src/test/java/esi/AppTest.java
@@ -9,6 +9,8 @@ public class AppTest {
 	@Test
 	public void test() {
 		assertEquals("Sample2", new App().sample());
+		assertEquals("Mike Sample", new App().sampleMike());
+		assertEquals("OutSystems dude - Is that a game?", new App().sampleMiguel());
 	}
 
 }
